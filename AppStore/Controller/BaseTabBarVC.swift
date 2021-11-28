dcddc23b59d13369d858ae53b05b683039a253ba
JeanCamargoReal <jeancamargoreal@icloud.com>
2021-11-28 14:39:43 -0300

    setting UITabBar

---
+++ AppStore/Controller/BaseTabBarVC.swift
@@ -0,0 +1,14 @@
+//
+//  BaseTabBarVC.swift
+//  AppStore
+//
+//  Created by Jean Camargo on 28/11/21.
+//
+
+import UIKit
+
+class BaseTabBarVC: UITabBarController {
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+}
