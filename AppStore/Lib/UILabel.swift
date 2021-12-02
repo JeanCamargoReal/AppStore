de4c0acec066cd217d44e94dc77760a47998f7f2
JeanCamargoReal <jeancamargoreal@icloud.com>
2021-12-02 16:39:29 -0300

    create label to title and company name

---
+++ AppStore/Lib/UILabel.swift
@@ -0,0 +1,20 @@
+//
+//  UILabel.swift
+//  AppStore
+//
+//  Created by Jean Camargo on 02/12/21.
+//
+
+import UIKit
+
+extension UILabel {
+	static func textLabel(text: String, fontSize: CGFloat, numberOfLines: Int = 1) -> UILabel {
+		let label = UILabel()
+		
+		label.font = UIFont.systemFont(ofSize: fontSize)
+		label.numberOfLines = numberOfLines
+		label.text = text
+		
+		return label
+	}
+}
--- AppStore/View/Busca/BuscaCell.swift
+++ AppStore/View/Busca/BuscaCell.swift
@@ -10,6 +10,8 @@ import UIKit
 class BuscaCell: UITableViewCell {
 	
 	let iconeImageView: UIImageView = .iconeImageView()
+	let tituloLabel: UILabel = .textLabel(text: "App nome", fontSize: 18, numberOfLines: 2)
+	let empresaLabel: UILabel = .textLabel(text: "Empresa nome", fontSize: 14)
 	
 	override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
 		super.init(style: style, reuseIdentifier: reuseIdentifier)
